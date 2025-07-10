dfe8a22978408547a62f18747df3f4b7d97c942f
김기현 <luke0985>
2025-07-10 21:36:25 +0900

    [feat] Shelf UI 연동 (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import { KEYS } from "./config/key";
 import LoginPage from "./pages/Auth/LoginPage";
 import SignupPage from "./pages/Auth/SignupPage";
 import ProfilePage from "./pages/Auth/ProfilePage";
+
+
 import BookStorySearchPage from "./pages/Main/BookStory/BookStorySearchPage";
 import SearchPage from "./pages/Main/SearchPage";
 import BookAddPage from "./pages/BookClub/Review/BookAddPage";
 
+import ShelfHomePage from "./pages/BookClub/Shelf/ShelfHomePage";
+import ShelfDetailPage from "./pages/BookClub/Shelf/ShelfDetailPage";
 const App = () => {
   return (
     <GoogleOAuthProvider clientId={KEYS.GOOGLE_CLIENT_ID}>
@@ -21,6 +25,13 @@ const App = () => {
           <Route path="/bookstory/search" element={<BookStorySearchPage />} />
           <Route path="/booksearch" element={<SearchPage />} />
           <Route path=":prefix/bookaddpage" element={<BookAddPage />} />
+
+
+          // 책장
+          <Route path=":prefix/shelf" element={<ShelfHomePage />} />
+          <Route path=":prefix/shelf/:shelfBookIndex" element={<ShelfDetailPage />} />
+
+          
         </Routes>
       </Router>
     </GoogleOAuthProvider>
