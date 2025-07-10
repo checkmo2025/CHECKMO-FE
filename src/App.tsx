abfbdb63da0bab890cdeed94d244f17132d2700e
DAYOUN1205 <dypark1205@hanmail.net>
2025-07-10 21:18:11 +0900

    app.tsx routes 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { KEYS } from "./config/key";
 import LoginPage from "./pages/Auth/LoginPage";
 import SignupPage from "./pages/Auth/SignupPage";
 import ProfilePage from "./pages/Auth/ProfilePage";
+import HomePage from './pages/BookClub/HomePage'      // 북클럽 홈
+import NoticePage from './pages/BookClub/NoticePage'  // 북클럽 공지사항
+import ClubSearchPage from './pages/Main/ClubSearchPage'  // 북클럽 검색
+import CreateClubPage from './pages/Main/CreateClubPage'
+
 
 const App = () => {
   return (
@@ -13,6 +18,19 @@ const App = () => {
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          
+          {/* /bookclub 이하에 북클럽 관련 페이지 묶기 */}
+          <Route path="/bookclub">
+            {/* /bookclub → 북클럽 홈 */}
+            <Route index element={<HomePage />} />
+
+            {/* /bookclub/notices → 공지사항 페이지 */}
+            <Route path="notices" element={<NoticePage />} />
+          </Route>
+
+          <Route path='/searchClub' element={<ClubSearchPage />} />
+
+          <Route path='/createClub' element={<CreateClubPage />} />
         </Routes>
       </Router>
     </GoogleOAuthProvider>
