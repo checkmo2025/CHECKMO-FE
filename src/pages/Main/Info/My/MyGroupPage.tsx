84de38dbd016f2cd6dd0922d5574753c6f63f27c
2jw0305 <ljw03053064@gmail.com>
2025-07-11 20:53:28 +0900

    [feat] 마이페이지 홈 구현

---
+++ src/pages/Main/Info/My/MyGroupPage.tsx
@@ -0,0 +1,5 @@
+const MyGroupPage = () => {
+  return <div>내 모임 페이지</div>;
+};
+
+export default MyGroupPage;
---
+++ src/pages/Main/Info/My/MySubscriptionPage.tsx
@@ -0,0 +1,5 @@
+const MySubscriptionPage = () => {
+  return <div>내 구독 페이지</div>;
+};
+
+export default MySubscriptionPage;
