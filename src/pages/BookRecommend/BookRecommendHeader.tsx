5da4d7c272b8fdddea828857676611225e3713f0
Oh jihwan <ojh3212@naver.com>
2025-07-14 17:13:01 +0900

    feat: 책 추천 카드 컴포넌트의 header 구현

---
+++ src/pages/BookRecommend/BookRecommendHeader.tsx
@@ -0,0 +1,29 @@
+import type { Member } from "../../types/bookRecommend";
+
+interface HeaderProps {
+  memberInfo: Member;
+}
+
+const BookRecommendHeader = ({ memberInfo }: HeaderProps) => {
+  return (
+    <>
+      <div className="flex flex-row mt-3 ml-3 mr-3">
+        <img
+          // src={memberInfo.imgUrl}
+          src="/profile.png"
+          className="mr-3 w-8 h-8
+                        sm:w-10 sm:h-10
+                        md:w-12 md:h-12
+                        lg:w-16 lg:h-16
+                        object-cover
+                        rounded-full"
+        />
+        <div className="flex items-center">
+          <h3>{memberInfo.nickName}</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookRecommendHeader;
