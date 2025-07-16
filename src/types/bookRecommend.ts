2cd158f0e1be7b2e680685e386340a29e344760a
Oh jihwan <ojh3212@naver.com>
2025-07-16 13:59:30 +0900

    refactor: type 네이밍 규칙 변경 및 공용 dto 분리

--- src/types/bookRecommend.ts
+++ src/types/bookRecommend.ts
@@ -1,45 +1,25 @@
-export type Book = {
-  bookId: number;
-  title: string;
-  author: string;
-  imgUrl: string;
-};
+import type { RecommendationDto } from "./dto";
 
-export type Member = {
-  memberId: number;
-  nickName: string;
-  imgUrl: string;
-};
-
-export type Recommendation = {
-  recommendId: number;
-  bookInfo: Book;
-  memberInfo: Member;
-  content: string; // 추천평 미리보기
-  rate: number;
-  createdAt: Date;
-};
-
-export type ListResult = {
-  recommendations: Recommendation[];
+export type RecommendListResult = {
+  recommendations: RecommendationDto[];
   hasNext: false;
   totalElements: number;
 };
 
-export type DetailResult = {
-  recommend: Recommendation;
+export type RecommendDetailResult = {
+  recommend: RecommendationDto;
   isMyRecommendation: boolean; // 내가 작성했는 지 여부에 따라 수정/삭제 버튼 띄우기
 };
 
-export type RecommendList = {
+export type RecommendListDto = {
   isSuccess: boolean;
   code: string;
   message: string;
-  result: ListResult;
+  result: RecommendListResult;
 };
 
-export type RecommendDetail = {
+export type RecommendDetailDto = {
   isSuccess: boolean;
   code: string;
-  result: DetailResult;
+  result: RecommendDetailResult;
 };
