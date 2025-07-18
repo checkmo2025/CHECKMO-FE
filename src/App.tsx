814f7f0d0ffa25bf2627bd2cb8079aa20fca5439
Oh jihwan <ojh3212@naver.com>
2025-07-18 17:25:53 +0900

    feat: 책 추천 관련 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { KEYS } from "./config/key"; 
+import { KEYS } from "./config/key";
 import LoginPage from "./pages/Auth/LoginPage";
 import SignupPage from "./pages/Auth/SignupPage";
 import ProfilePage from "./pages/Auth/ProfilePage";
+import { dummyRecommendList } from "./pages/BookRecommend/DummyRecommendList";
+import BookRecommendPage from "./pages/BookRecommend/BookRecommendPage";
+import BookRecommendDetailPage from "./pages/BookRecommend/BookRecommendDetailPage";
+import BookRecommendCreatePage from "./pages/BookRecommend/BookRecommendCreatePage";
 
 const App = () => {
+  const dummyList = dummyRecommendList;
+
   return (
     <GoogleOAuthProvider clientId={KEYS.GOOGLE_CLIENT_ID}>
       <Router>
@@ -13,6 +19,18 @@ const App = () => {
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route
+            path="/recommend"
+            element={<BookRecommendPage bookRecommendList={dummyList} />}
+          />
+          <Route
+            path="/recommend/:recommendId"
+            element={<BookRecommendDetailPage />}
+          />
+          <Route
+            path="/recommend/create/:bookId"
+            element={<BookRecommendCreatePage />}
+          />
         </Routes>
       </Router>
     </GoogleOAuthProvider>
