2ec8ae453625bbe12d98e7ae72ed9dce240b0a76
김기현 <luke0985>
2025-08-01 16:07:47 +0900

    feat API 구조 + 검색기능 API 연결 (#111)

---
+++ src/pages/BookRecommend/BookRecommendSearchPage.tsx
@@ -0,0 +1,44 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useCallback } from "react";
+import BookSearch from "../../components/Search/BookSearch";
+import type { SearchBook } from "../../types/BookSearchdto";
+import Header from "../../components/Header";
+
+const BookRecommendSearchPage = () => {
+  const navigate = useNavigate();
+  const { bookclubId } = useParams<{ bookclubId: string }>();
+
+  const handleBookSelect = useCallback(
+    (book: SearchBook) => {
+      navigate(`/bookclub/${bookclubId}/recommend/${book.isbn}/create`);
+    },
+    [navigate, bookclubId]
+  );
+
+  const actions = [
+    {
+      label: "추천하기",
+      onClick: handleBookSelect,
+      className: "bg-[var(--button-brown,#A6917E)] text-white",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col h-full mx-10 my-7">
+      <Header
+        pageTitle="책 추천하기"
+        userProfile={{
+          username: "오즈",
+          bio: "re_turnto_oz",
+        }}
+        notifications={[]}
+        customClassName="mb-10"
+      />
+      <div className="flex-grow">
+        <BookSearch SearchResultHeight={287} actions={actions} />
+      </div>
+    </div>
+  );
+};
+
+export default BookRecommendSearchPage;
