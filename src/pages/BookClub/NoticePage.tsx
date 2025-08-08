f6eca02c81525feda3ea664340a7f3ab6e75420d
DAYOUN1205 <dypark1205@hanmail.net>
2025-08-08 18:35:34 +0900

    메인컨텐츠 및 헤더 간격 디테일 수정

--- src/pages/BookClub/NoticePage.tsx
+++ src/pages/BookClub/NoticePage.tsx
@@ -119,17 +119,18 @@ export default function HomePage(): React.ReactElement {
   ];
 
   return (
-    <div className="absolute left-[315px] right-[42px] opacity-100 h-screen flex flex-col">
-      <Header pageTitle={'공지사항'} userProfile={{
-        username: 'dayoun',
-        bio: '아 피곤하다.'
-      }} 
-      notifications={[]}
-      customClassName="mt-[30px]"
+    <div className="w-full h-screen flex flex-col">
+        <Header pageTitle={'공지사항'} userProfile={{
+          username: 'dayoun',
+          bio: '아 피곤하다.'
+        }} 
+        notifications={[]}
+        customClassName="mt-[30px] ml-[52px] mr-[41px]"
       />
 
       {/* 메인 컨텐츠 - 남은 공간을 모두 사용하며 스크롤 */}
-      <div className="flex-1 overflow-y-auto mt-[15px] ml-[52px] mr-4 pb-8">
+      <div className="flex-1 overflow-y-auto ml-[52px] mr-4">
+        <div className="mt-[15px]">
         {/* 상단: 중요 공지사항 */}
         <section className="mb-6">
           <AnnouncementCard items={dummyAnnouncements.slice(0, 5)} />
@@ -139,6 +140,7 @@ export default function HomePage(): React.ReactElement {
         <section className="mt-[43px]">
           <AnnouncementList items={listItems} />
         </section>
+        </div>
       </div>
     </div>
   );
