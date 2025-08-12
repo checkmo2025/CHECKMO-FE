b2f209ec49e702abce4c8b841ad07f7fcb174c5f
2jw0305 <ljw03053064@gmail.com>
2025-08-13 03:12:11 +0900

    feat: 헤더 type 파일

---
+++ src/types/header.ts
@@ -0,0 +1,31 @@
+export type NotificationPreviewItem = {
+  notificationId: number;
+  notificationType: string; 
+  senderNickname: string;
+  read: boolean;
+  createdAt: string;   
+  redirectPath: string;
+};
+export type NotificationPreviewResult = {
+  notifications: NotificationPreviewItem[];
+};
+
+export type MyProfileResult = {
+  nickname: string;
+  description: string;
+  profileImageUrl: string;
+  categories?: { id: number; name: string }[];
+};
+
+// UI용 뷰모델
+export type HeaderUserProfile = {
+  username: string;
+  bio: string;
+  imgUrl?: string;
+};
+
+export type HeaderNotification = {
+  message: string;
+  time: string;
+  redirectPath?: string;
+};
