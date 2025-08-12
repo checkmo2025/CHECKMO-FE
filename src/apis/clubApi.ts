0393b4ea8f389b7f8e5752e072d4768a05a9eb2d
DAYOUN1205 <dypark1205@hanmail.net>
2025-08-12 12:13:07 +0900

    fix: 충돌 코드 수정

--- src/apis/clubApi.ts
+++ src/apis/clubApi.ts
@@ -1,15 +1,9 @@
 import type { ClubDto, CreateClubRequestDto } from "../types/bookClub";
-import type { ClubDto } from "../types/dto";
 import { axiosInstance } from "./axiosInstance";
 
 // 클럽 생성
 export const createClub = async (clubData: CreateClubRequestDto): Promise<ClubDto> => {
   return axiosInstance.post('/clubs', clubData) as Promise<ClubDto>;
-
-// 클럽 목록 조회
-export const getClubList = async (): Promise<ClubDto[]> =>
-  axiosInstance.get<ClubDto[]>("/api/clubs").then((r) => r.data);
-};
 
 // 클럽 상세 조회
 export const getClubDetail = async (clubId: number): Promise<ClubDto> =>
