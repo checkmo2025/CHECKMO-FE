0f51956f66b5ba3c7df4d119f8d0e93f3269907b
DAYOUN1205 <dypark1205@hanmail.net>
2025-08-12 11:34:04 +0900

    fix: 반환 타입 수정해서 isAvailable 항상 false인 버그 해결

--- src/apis/clubValidationApi.ts
+++ src/apis/clubValidationApi.ts
@@ -3,9 +3,10 @@ import { axiosInstance } from './axiosInstance';
 // 독서모임 이름 중복검사
 // 반환값: true(중복), false(사용 가능)
 export const validateClubName = async (clubName: string): Promise<boolean> => {
-  return axiosInstance.get(`/clubs/checkName`, {
+  const response = await axiosInstance.get<boolean>(`/clubs/checkName`, {
     params: {
       clubName,
     },
   });
+  return response.data;
 };
