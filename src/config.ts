c914a6d91ff9f651a56e3633243e5714139e91a7
2jw0305 <ljw03053064@gmail.com>
2025-08-12 19:18:38 +0900

    chore: 라우트 경로 수정

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ if (!API_BASE_URL) {
   throw new Error("VITE_API_URL is missing in .env");
 }
 
-export const GOOGLE_OAUTH_URL = `${API_BASE_URL}/oauth2/authorization/google`;
-export const KAKAO_OAUTH_URL  = `${API_BASE_URL}/oauth2/authorization/kakao`;
+export const GOOGLE_OAUTH_URL = `${API_BASE_URL}oauth2/authorization/google`;
+export const KAKAO_OAUTH_URL  = `${API_BASE_URL}oauth2/authorization/kakao`;
 
 export { API_BASE_URL };
