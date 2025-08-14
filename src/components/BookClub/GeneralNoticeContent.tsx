b30c20a4962c5325a176bc11874c064c7f17f4d0
DAYOUN1205 <dypark1205@hanmail.net>
2025-08-14 16:40:55 +0900

    refactor: 일반 공지 상페이  타입 안정성 보완

--- src/components/BookClub/GeneralNoticeContent.tsx
+++ src/components/BookClub/GeneralNoticeContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { generalNoticeItemDto } from '../../types/clubNotice';
 
 interface GeneralNoticeContentProps {
-  data: generalNoticeItemDto | any;
+  data: generalNoticeItemDto;
 }
 
 export default function GeneralNoticeContent({ data }: GeneralNoticeContentProps): React.ReactElement {
