930f041f7c3c5ef8f1bca3d70c0aa7a4cd7055c7
2jw0305 <ljw03053064@gmail.com>
2025-08-15 03:08:12 +0900

    feat: 팔로잉 안 한 사용자 팔로잉 API 추가

--- src/apis/otherApi.ts
+++ src/apis/otherApi.ts
@@ -5,3 +5,8 @@ import type { OtherProfile } from "../types/other";
 export async function getOtherProfile(memberNickname: string): Promise<OtherProfile> {
   return axiosInstance.get<unknown, OtherProfile>(`/members/${memberNickname}`);
 }
+
+/** 특정 회원 팔로우: POST /api/members/{memberNickname}/following */
+export async function followMember(memberNickname: string): Promise<void> {
+  return axiosInstance.post(`/members/${memberNickname}/following`);
+}
