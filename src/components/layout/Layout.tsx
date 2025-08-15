ab618846294a777b5d827eccf831ca5cf8c9fc1d
김기현 <luke0985>
2025-08-16 03:36:53 +0900

    style : 검색페이지

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <main className="flex-1 overflow-y-auto bg-white">
+      <main className="flex-1 bg-white">
         <Outlet />
       </main>
     </div>
