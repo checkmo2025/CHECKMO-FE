5f8e94b299b62d166e487d76da33b209bcc6a22d
returntooz <ojh3212@naver.com>
2025-08-20 16:38:36 +0900

    style: TopicCard 닉네임 칸 길이 고정

--- src/components/Meeting/TopicCard.tsx
+++ src/components/Meeting/TopicCard.tsx
@@ -9,16 +9,18 @@ interface TopicCardProps {
 const TopicCardComponent = ({ content, authorInfo }: TopicCardProps) => {
   return (
     <div className="flex flex-1 min-w-[500px] items-center justify-between bg-[#F4F2F1] p-1 rounded-2xl border-2 border-[#EAE5E2]">
-      <div className="flex items-center space-x-2 flex-1 overflow-hidden">
+      <div className="flex items-center gap-3 flex-1 overflow-hidden">
         <img
           src={authorInfo.profileImageUrl}
           alt={authorInfo.nickname}
-          className="w-8 h-8 mx-2 rounded-full bg-gray-200 flex-shrink-0"
+          className="w-8 h-8 rounded-full bg-gray-200 shrink-0 ml-2"
         />
-        <span className="text-black text-[12px] font-medium whitespace-nowrap ml-5 mr-20">
+        <span className="text-black text-[12px] font-medium w-[120px] shrink-0 overflow-hidden text-ellipsis whitespace-nowrap">
           {authorInfo.nickname}
         </span>
-        <p className="text-black text-[12px] font-medium truncate">{content}</p>
+        <p className="text-black text-[12px] font-medium truncate min-w-0">
+          {content}
+        </p>
       </div>
     </div>
   );
