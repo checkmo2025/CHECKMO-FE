7aa7d1eef32145b0c7da6a09390b98a2323ae6dd
returntooz <ojh3212@naver.com>
2025-08-20 09:44:01 +0900

    feat: 책 상세 검색 API 연동

--- src/apis/BookSearch/search.ts
+++ src/apis/BookSearch/search.ts
@@ -1,10 +1,18 @@
-import { axiosInstance } from '../axiosInstance';
-import type { BookSearchResult } from '../../types/BookSearchdto';
+import { axiosInstance } from "../axiosInstance";
+import type { BookSearchResult, SearchBook } from "../../types/BookSearchdto";
 
 // 책검색 데이터 가져오기
-export function SearchBooks(keyword: string, page: number = 1): Promise<BookSearchResult> {
-  return axiosInstance.get('/books/search', { 
-    params: { keyword, page } 
-  }
-  );
+export function SearchBooks(
+  keyword: string,
+  page: number = 1
+): Promise<BookSearchResult> {
+  return axiosInstance.get("/books/search", {
+    params: { keyword, page },
+  });
 }
+
+// 책 검색 상세 조회
+export const getBookDetail = async (isbn: string): Promise<SearchBook> => {
+  const response: SearchBook = await axiosInstance.get(`/books/${isbn}`);
+  return response;
+};
--- src/hooks/BookSearch/useBookSearch.ts
+++ src/hooks/BookSearch/useBookSearch.ts
@@ -1,13 +1,23 @@
-import { useQuery } from '@tanstack/react-query';
-import { SearchBooks } from '../../apis/BookSearch/search';
-import type { BookSearchResult } from '../../types/BookSearchdto';
+import { useQuery } from "@tanstack/react-query";
+import { getBookDetail, SearchBooks } from "../../apis/BookSearch/search";
+import type { BookSearchResult } from "../../types/BookSearchdto";
 
-export function useBookSearch(keyword: string, page: number ) {
-
+export function useBookSearch(keyword: string, page: number) {
   return useQuery<BookSearchResult, Error>({
-    queryKey: ['bookSearch', keyword, page],
-    queryFn: () => {return SearchBooks(keyword, page);},
-    enabled: keyword.trim().length > 0,   // 빈 문자열일 땐 호출 금지
-    staleTime: 1000 * 60 * 5,           // 5분 동안 캐시 유지
+    queryKey: ["bookSearch", keyword, page],
+    queryFn: () => {
+      return SearchBooks(keyword, page);
+    },
+    enabled: keyword.trim().length > 0, // 빈 문자열일 땐 호출 금지
+    staleTime: 1000 * 60 * 5, // 5분 동안 캐시 유지
   });
 }
+
+export const useGetBookDetail = (isbn: string) => {
+  return useQuery({
+    queryKey: ["bookDetail", isbn],
+    queryFn: () => getBookDetail(isbn),
+    enabled: !!isbn,
+    staleTime: 1000 * 60 * 5, // 5분
+  });
+};
