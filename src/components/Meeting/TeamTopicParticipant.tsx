f6c9b09919e86aa979e64239d5b78bd6a0afb56c
returntooz <ojh3212@naver.com>
2025-08-20 16:34:20 +0900

    refactor: API 연동을 위한 ParticipantProps 구조 변경

--- src/components/Meeting/TeamTopicParticipant.tsx
+++ src/components/Meeting/TeamTopicParticipant.tsx
@@ -1,8 +1,8 @@
-import type { MemberDto } from "../../types/dto";
+import type { AuthorDto } from "../../types/dto";
 
 interface TeamTopicParticipantProps {
-  teamName: string;
-  participants: MemberDto[];
+  teamName: number;
+  participants: AuthorDto[];
 }
 
 const TeamTopicParticipant = ({
@@ -11,21 +11,21 @@ const TeamTopicParticipant = ({
 }: TeamTopicParticipantProps) => {
   return (
     <>
-      <h2 className="text-base font-semibold mb-3">{`${teamName}조`} 참여자</h2>
-      <div className="max-w-[250px] pt-2 pb-4 px-3 border-2 border-[#D6E5CE] rounded-xl">
+      <h2 className="text-base font-semibold mb-3">{String.fromCharCode(64 + teamName)}조 참여자</h2>
+      <div className="max-w-[400px] pt-2 pb-4 px-3 border-2 border-[#D6E5CE] rounded-xl">
         <ul className={`divide-y-2 divide-gray-200 overflow-auto`}>
           {participants.map((p) => (
             <li
-              key={p.memberId}
+              key={p.nickname}
               className="flex items-center mt-2 p-2 border-b-2 border-gray-200"
             >
               <img
-                src={p.imgUrl ?? "/default-avatar.png"}
-                alt={p.nickName}
+                src={p.profileImageUrl ?? "/default-avatar.png"}
+                alt={p.nickname}
                 className="w-8 h-8 rounded-full bg-gray-200 flex-shrink-0"
               />
               <span className="ml-3 text-md font-medium text-black">
-                {p.nickName}
+                {p.nickname}
               </span>
             </li>
           ))}
