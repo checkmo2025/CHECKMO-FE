53814da402b2ed8d20a6640097d4cad0508ec1c6
2jw0305 <ljw03053064@gmail.com>
2025-08-22 01:30:04 +0900

    style: 마이홈페이지 글씨 변경

--- src/pages/Main/Info/My/MyHomePage.tsx
+++ src/pages/Main/Info/My/MyHomePage.tsx
@@ -36,7 +36,7 @@ const MyPage = () => {
       case "LIKE":
         return `${item.senderNickname} 님이 내 책이야기에 좋아요를 눌렀습니다.`;
       case "FOLLOW":
-        return `${item.targetName}이 팔로잉을 시작했습니다.`;
+        return `${item.targetName}님이 구독했습니다.`;
       case "JOIN_CLUB":
         return `${item.targetName}에 가입되셨습니다.`;
       default:
