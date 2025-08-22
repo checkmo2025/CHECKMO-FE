d017c889222c40df7e195980067590d32cdc7500
오지환 <116332741+returntoOZ@users.noreply.github.com>
2025-08-22 19:50:53 +0900

    Revert "[refactor] 라우터 파일 분리 & build 번들 최적화"

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
   return {
     plugins: [react(), tailwindcss()],
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ["react", "react-dom"],
-            vendor: ["axios", "date-fns"],
-          },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    preview: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
         },
       },
     },
